ed0298bed5c55d173b49fb67fcff1188e94268b4
sonamahapatra <123800013+sonamahapatra@users.noreply.github.com>
2023-04-06 08:36:56 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to 276 Batch");
+            Console.WriteLine("Adding in Remote");
             Console.ReadLine();
         }
     }
