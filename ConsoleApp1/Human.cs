4cf80fedf2f7dc1af606134d29164d1113a77d7c
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-06 15:51:36 +0530

    [sonamahapatra] Added Human

---
+++ ConsoleApp1/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public  class Human
+    {
+        public string name;
+        public int age;
+        public float height, weight;
+
+        public void Speek()
+        {
+            Console.WriteLine("Human can speak");
+        }
+        public void HumanDetails(string humanName, float humanHeight,float humanWeight,int age)
+        {
+            Console.WriteLine("HumanDetails:{0} {1} {2} {3}", humanName,humanHeight,humanWeight,age);
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to 276 Batch");
-            Console.WriteLine("modifing to local");
+            //Console.WriteLine("Welcome to 276 Batch");
+            //Console.WriteLine("modifing to local");
+            Human human = new Human();
+            Console.WriteLine("Please Enter Human Name:");
+            human.name = Console.ReadLine();
+            Console.WriteLine("Enter Your Age:");
+            human.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Height:");
+            human.height =Convert.ToSingle( Console.ReadLine());
+            Console.WriteLine(" Enter Your Weight:");
+            human.weight = Convert.ToSingle(Console.ReadLine());
+
             Console.ReadLine();
         }
     }
